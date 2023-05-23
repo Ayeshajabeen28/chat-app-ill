48d1f1fc0d07d7b6e14f8e27c1f9f7b506967026
Ayesha jabeen <114102646+Ayeshajabeen28@users.noreply.github.com>
2023-05-23 12:35:52 +0530

    Initial commit

---
+++ objects.js
@@ -0,0 +1,159 @@
+//object literals
+// let user = {
+//     name : 'crystal',
+//     age : 30,
+//     email : 'crysral@mail.com',
+//     location :'berlin',
+//     blogs:['why mac & cheese rules',' 10 things to make with marmite']
+// };
+// console.log(user);
+// console.log(user,name);
+// //user.age = 35;
+// console.log(user.age);
+// //const key = 'location';
+// console.log(user['location']);
+// user['name']= 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+//object literals
+//const blogs =[
+    // {title:'why maac & cheese rules',likes:30},
+    // {title: '10 things to make with marmite', likes:50}
+//];
+//console.log(blogs);
+
+
+// let user = {
+//     name : 'crystal',
+//     age : 30,
+//     email : 'crysral@mail.com',
+//     location :'berlin',
+//     blogs:[
+//         {title:'why maac & cheese rules',likes:30},
+//     {title: '10 things to make with marmite', likes:50}
+//     ],
+//     login(){
+//      console.log('the user logged in');
+//     },
+//     logout(){
+//         console.log('the user logged out');
+
+//     },
+//     logblogs: function () {
+//         // //console.log(this);
+//         console.log('this user ha written the following blogs:');
+//         this.blogs.forEach(blogs => {
+//             console.log(blogs);
+
+
+//         });
+//         console.log(this);
+        
+//     }
+// };
+// user.login();
+// user.logout();
+// user.logblogs();
+// console.log(this);
+// const name ='ayesha';
+// name.toUpperCase();
+
+
+//math objects
+
+
+// console.log(Math);
+// console.log(Math.E);
+// console.log(MATH.PI);
+// //Console.log(Math.PI);
+// Console.log(Math.E);
+// const area =7.7;
+// Console.log(Math.round(area));
+// Console.log(Math.floor(area));
+// Console.log(Math.ceil(area));
+// Console.log(Math.trunc(area));
+// //random numbers
+
+// const random = Math.random();
+
+// console.log(random);
+// console.log(Math.round(random*100));
+
+//primitive values
+// let scoreone =50;
+// let scoreTwo = scoreone;
+// console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
+// scoreone =100;
+// console.log('scoreone: ${scoreone}','scoretwo:${scoretwo}');
+ 
+
+// const para = document.querySelector('p');
+
+// console.log(para);
+// const para = document.querySelector('.error');
+
+// console.log(para);
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// const para = document.querySelector('body > h1');
+
+// console.log(para);
+//const para = document.querySelectorAll('p');
+//para.forEach(para => 
+//console.log(para)
+//const document.querySelector('.error');
+
+//console.log(para[0]);
+//console.log(error);
+
+// get an element by ID
+
+// const title = document.getElementbyId('page-title');
+// console.log(title);
+
+// //get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+//get elements by their tag name
+
+
+
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[0]);
+
+//const para = document.querySelector('p');
+
+//console.log(para.innerText);
+//para.innerText = 'ninjas are awesome!';
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para =>{
+
+//     console.log(para.innertext);
+//     para.innerText +='new text';
+// });
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+
+// link.setAttribute('href', 'https://www.theayesha.com');
+// link.innertext = 'theayesha website';
+
+// const mssg = document.querySelector('p');
+
+
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class','success');
+// mssg.setAttribute('style','color:red');
+
+//const title = document.querySelector('h1');
+
+//title.setAttribute('style','margin50px;');
